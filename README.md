06dda53331f50519aabc4c49ef89371c82f88bc8
DevAshleyD <47677476+DevAshleyD@users.noreply.github.com>
2019-11-05 14:41:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Animated SVG Weather Icons
+
+A showcase of open source animated SVG icons I created.
+View demo through gh-pages at http://devashleyd.github.io/animated_weather_svgs/
---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* Pick your poison */
+/* 
+// Document Ready no jQuery
+document.addEventListener("DOMContentLoaded", function(event) { 
+  "use strict";
+  
+});
+*/
+
+/* 
+// Document Ready Using jQuery
+$(function() {
+  "use strict";
+  
+});
+*/
